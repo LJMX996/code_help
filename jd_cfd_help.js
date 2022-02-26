88e4b0d5a51b5267b8530e87e0d8434269abf608
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2022-02-27 00:26:24 +0800

    jd_cfd_help.js

--- jd_cfd_help.js
+++ jd_cfd_help.js
@@ -25,9 +25,9 @@ $.result = [];
 $.shareCodes = [];
 let cookiesArr = [], cookie = '', token = '';
 let UA, UAInfo = {};
-let nowTimes;
 const randomCount = $.isNode() ? 20 : 3;
-$.appId = "92a36";
+$.appId = 10032;
+let lnrun = 0;
 function oc(fn, defaultVal) {//optioanl chaining
   try {
     return fn()
@@ -77,15 +77,22 @@ if ($.isNode()) {
       $.allTask = []
       $.info = {}
       token = await getJxToken()
-      await cfd();
-      await $.wait(3000);
+      lnrun++;
+	  await cfd();
+	  await $.wait(2000);
+      if (lnrun == 5) {
+              console.log(`\n【访问接口次数达到5次，休息半分钟.....】\n`);
+              await $.wait(30 * 1000);
+              lnrun = 0;
+			}
     }
   }
   let res = await getAuthorShareCode('')
   $.strMyShareIds = [...(res || [])]
   await shareCodesFormat()
   for (let i = 0; i < cookiesArr.length; i++) {
-    cookie = cookiesArr[i];
+    lnrun++;
+	cookie = cookiesArr[i];
     $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
     $.canHelp = true
     UA = UAInfo[$.UserName]
@@ -94,7 +101,7 @@ if ($.isNode()) {
       for (let j = 0; j < $.newShareCodes.length && $.canHelp; j++) {
         console.log(`账号${$.UserName} 去助力 ${$.newShareCodes[j]}`)
         $.delcode = false
-        await helpByStage($.newShareCodes[j])
+		await helpByStage($.newShareCodes[j])
         await $.wait(2000)
         if ($.delcode) {
           $.newShareCodes.splice(j, 1)
@@ -105,6 +112,11 @@ if ($.isNode()) {
     } else {
       break
     }
+	if (lnrun == 5) {
+        console.log(`\n【访问接口次数达到5次，休息半分钟.....】\n`);
+        await $.wait(30 * 1000);
+        lnrun = 0;
+	}
   }
   await showMsg();
 })()
@@ -1451,7 +1463,7 @@ function shareCodesFormat() {
     } else {
       $.newShareCodes = [...new Set([...$.shareCodes, ...$.strMyShareIds])];
     }
-    console.log(`您将要助力的好友${JSON.stringify($.newShareCodes)}`)
+    console.log(`\n\n您将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
   })
 }
@@ -1552,7 +1564,7 @@ async function requestAlgo() {
       'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7'
     },
     'body': JSON.stringify({
-      "version": "3.1",
+      "version": "1.0",
       "fp": $.fingerprint,
       "appId": $.appId.toString(),
       "timestamp": Date.now(),
